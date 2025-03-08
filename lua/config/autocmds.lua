ac1eca042bdb9c64904b9bea817cbcd29ea673e1
sayem.afm <afm.sayem@gmail.com>
2025-03-08 07:57:05 +0600

    chore: updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+local cmd = vim.cmd
+
+-- remove whitespace on save
+cmd([[au BufWritePre * :%s/\s\+$//e]])
+
+-- don't auto commenting new lines
+cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+
+-- ain files are toml files
+cmd([[au BufReadPost *.ain set syntax=toml]])
+
+-- register eta files for parsing
+vim.treesitter.language.register("embedded_template", "eta") -- the someft filetype will use the python parser and queries.
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -10,9 +10,10 @@ return {
     auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20241022",
+      -- model = "claude-3-5-sonnet-20241022",
+      model = "claude-3-7-sonnet-20250219",
       temperature = 0,
-      max_tokens = 4096,
+      max_tokens = 8192,
     },
     ---Specify the special dual_boost mode
     ---1. enabled: Whether to enable dual_boost mode. Default to false.
