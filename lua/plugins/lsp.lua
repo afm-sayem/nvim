3fb552949e60ab1eaea9999ad7b0c66607f8f7ab
sayem.afm <afm.sayem@gmail.com>
2025-03-08 23:24:26 +0600

    chore: add vue support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-lspconfig",
+  opts = {
+    setup = {
+      tsserver = function(_, opts)
+        local mason_registry = require("mason-registry")
+        local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+          .. "/node_modules/@vue/language-server"
+
+        opts.init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_language_server_path,
+              languages = { "vue" },
+            },
+          },
+        }
+        opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+      end,
+    },
+  },
+}
